1a50ddcf9f6b555101102b63ba55365adfef9334
rosslasco <123819315+rosslasco@users.noreply.github.com>
2023-03-24 01:43:54 +0800

    Update streamlit_app.py
    added csv serializer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st;
+import pandas;
 
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Favorites')
@@ -9,3 +10,7 @@ st.text('🥑🍞 Avocado Toast')
 
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
