80217d2ff271de515bcc66e997465542af4c918c
rosslasco <123819315+rosslasco@users.noreply.github.com>
2023-03-24 01:36:11 +0800

    Update streamlit_app.py
    added a text recipe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-import streamlit;
+import streamlit as st;
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+st.title('My Parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
