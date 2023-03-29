5596537739309f17ae7d6b707dcde100d5c058a2
rosslasco <123819315+rosslasco@users.noreply.github.com>
2023-03-30 02:00:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import pandas;
 import requests;
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -21,6 +24,7 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 # Display the table on the page.
 fruit_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruit_to_show)
+my_cur.execute("insert into fruit_load_list values ('" + my_fruit_list + "');")
 
 
 
@@ -28,6 +32,7 @@ st.header("Fruityvice Fruit Advice!")
 # st.text(fruityvice_response.json())
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values ('" + fruit_choice + "');")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # write your own comment -what does the next line do? deserialize json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -35,8 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
